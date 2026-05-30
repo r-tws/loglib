37f980c15e042a2145c6d92287e5da8fd5ac66c6
rt‎ ‎ <1337dopamine@proton.me>
2026-05-30 15:50:08 +0200

    a

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ function log:print(msg,lvl)
 lvl=lvl or log.INFO
 local m="["..lvl.."] "..tostring(msg)
 if lvl==log.WARN then warn(m)
-elseif lvl==log.ERROR then task.spawn(error,m,0)
+elseif lvl==log.ERROR then task.spawn(function()error(m,0)end)
 elseif lvl==log.INFO then ts:Message(m)
 else print(m) end
 end
